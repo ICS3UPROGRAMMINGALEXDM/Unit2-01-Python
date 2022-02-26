fda2e21d164ba152304793065598d97db36d3b32
Ubuntu <ubuntu@ip-172-31-86-29.ec2.internal>
2022-02-26 19:00:42 +0000

    Finished area_per_circle.py

---
+++ area_per_circle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# Created By: Alex De Meo
+# Date: 02//22
+# Description: displays the area and perimeter of a circle with a radius of 15
+import math
+
+rad = 15
+
+circ = math.pi * 2 * rad
+
+
+area = math.pi * (rad**2)
+
+print("The area of a circle with a radius of 15 is {}".format(area))
+print("The circumeference of a circle witha a radiue of 15 is {}".format(circ))
